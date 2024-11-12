3c1e157f9f6af2722bc92e65b24139ebda166a07
saket110905 <saket.kumar@emerson.com>
2024-11-12 11:04:40 +0530

    Commit

--- scripts/Main.js
+++ scripts/Main.js
@@ -6,6 +6,7 @@ define("EmersonTest/scripts/Main", [
             onLoad: function () {
                 alert("widget Loaded");
                 this.getCSRFToken();
+                alert("csrf token received"+csrfToken);
                 document.getElementById("importbtn").addEventListener("click", this.uploadPart);
             },
             updateWidget: function () {t
