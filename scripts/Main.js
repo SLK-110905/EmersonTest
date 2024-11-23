0bdc05d300f6c0ab448c61083245a95e42532762
saket110905 <saket.kumar@emerson.com>
2024-11-23 21:08:48 +0530

    Initial Message

--- scripts/Main.js
+++ scripts/Main.js
@@ -1,31 +1,21 @@
-require.config({
-    paths: {
-        reactproduction: "https://unpkg.com/react@18/umd/react.production.min.js",
-        reactdomproduction:"https://unpkg.com/react-dom@18/umd/react-dom.production.min.js",
-        reactbabel:"https://unpkg.com/babel-standalone@6/babel.min.js"
-    }
-});
 define("EmersonTest/scripts/Main", [
-    "DS/WAFData/WAFData",
-    "EmersonTest/scripts/Main/reactdomproduction",
-    "EmersonTest/scripts/Main/reactproduction",
-    "EmersonTest/scripts/Main/reactbabel"
+    "DS/WAFData/WAFData"
 ],
     function (WAFData) {
         let myWidget = {
             onLoad: function () {
                 alert("widget has been Loaded");
-                const App = () => {
-                    return (
-                        <div>
-                            <h1>Hello, React from CDN!</h1>
-                            <p>This is a simple React app running through CDN.</p>
-                        </div>
-                    );
-                };
+                // const App = () => {
+                //     return (
+                //         <div>
+                //             <h1>Hello, React from CDN!</h1>
+                //             <p>This is a simple React app running through CDN.</p>
+                //         </div>
+                //     );
+                // };
             
                 // Render the React component to the DOM
-                ReactDOM.render(<App />, document.getElementById('root'));
+                //ReactDOM.render(<App />, document.getElementById('root'));
                 
             },
             updateWidget: function () {
