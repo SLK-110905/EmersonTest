c24f7981ef941043496379069030032ecff6eca0
saket110905 <saket.kumar@emerson.com>
2024-11-25 17:50:38 +0530

    commit message

--- scripts/Main.js
+++ scripts/Main.js
@@ -9,17 +9,8 @@ define("EmersonTest/scripts/Main", ["DS/WAFData/WAFData",'babel',"react-dom","re
 let myWidget = {
     onLoad: function () {
     alert("debug 2");
-    const App = () => {
-        return (
-        <div>
-            <h1>Hello, React from CDN!</h1>
-            <p>This is a simple React app running through CDN.</p>
-        </div>
-        );
-    };
-
       // Create a root and render the React component to the DOM
-      ReactDOM.render(React.createElement(App), document.getElementById('root'));
+      ReactDOM.render(React.createElement("<h1>Hello from react"), document.getElementById('root'));
     },
     updateWidget: function () {},
 };
