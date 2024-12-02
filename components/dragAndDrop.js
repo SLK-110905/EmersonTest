4b84f8e3ed18de5287424c6e59bec1f5cdcf0c00
saket110905 <saket.kumar@emerson.com>
2024-12-02 14:36:11 +0530

    commit message

--- components/dragAndDrop.js
+++ components/dragAndDrop.js
@@ -535,7 +535,7 @@ define("EmersonTest/components/dragAndDrop", ["DS/DataDragAndDrop/DataDragAndDro
                  //For getting display Name for Maturity State
                 console.log("Inside getStateValue: ");
                 return new Promise((onMyResolve,onMyFailure) => {
-                WAFData.authenticatedRequest(`https://oi000186152-us1-space.3dexperience.3ds.com/enovia/resources/v1/modeler/documents${partId}`, {
+                WAFData.authenticatedRequest(`https://oi000186152-us1-space.3dexperience.3ds.com/enovia/resources/v1/modeler/documents/${partId}`, {
                     method: "GET",
                     timeout:15000,
                     onComplete: function(forstateRes, headerforStateRes) {
